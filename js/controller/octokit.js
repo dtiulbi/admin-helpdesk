207b342e5ac9ee90cf76e3ca240f004c52c0181d
christyuda <yudacihuy09@gmail.com>
2023-06-15 00:39:51 +0700

    nothing

--- js/controller/octokit.js
+++
@@ -1,7 +0,0 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
-
-export let nekot = 'Z2hwX2lmVW94RGVyaWdxWDdicXg4RzdFNklMczlQTUtGVTJnaVlZZg==';
-
-export let octokit = new Octokit({
-  auth: atob(nekot)
-});
